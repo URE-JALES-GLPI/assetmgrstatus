09d66b2b64c8b1bd79213136097fdaef8352b28c
Leo Poiatti <leopoiatti00@gmail.com>
2026-08-18 15:53:05 -0300

    transfer.php: lista categorias sem filtro (tabela não tem is_deleted nessa versão)

--- front/transfer.php
+++ front/transfer.php
@@ -265,7 +265,6 @@ Html::header('Transferência', $_SERVER['PHP_SELF'], 'tools', 'assetmgrstatus', 
                         <?php
                         $tcats = $DB->request([
                             'FROM'  => 'glpi_itilcategories',
-                            'WHERE' => ['is_deleted' => 0],
                             'ORDER' => ['completename ASC'],
                         ]);
                         foreach ($tcats as $tcat): ?>
